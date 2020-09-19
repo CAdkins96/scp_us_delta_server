606f3a5066cb56c9c052f1603d810b21db5a3e00
Chris Adkins <adkins1118@marshall.edu>
2020-09-18 23:57:26 -0400

    initial commit

---
+++ build/precache-manifest.343a6dbac541d7eb8d36d09987cbfa05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b112e633da0edcfe46fe2526121fa2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2594899ea61b53c96d0d",
+    "url": "/static/css/main.437ff074.chunk.css"
+  },
+  {
+    "revision": "f1d5729a25f419f8456f",
+    "url": "/static/js/2.dd2a4d4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dd2a4d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2594899ea61b53c96d0d",
+    "url": "/static/js/main.fbdc0790.chunk.js"
+  },
+  {
+    "revision": "c6732150a15897ed0f8d",
+    "url": "/static/js/runtime-main.aee450ce.js"
+  },
+  {
+    "revision": "813e3519e7eb3da7c4fc6e87875a9b30",
+    "url": "/static/media/CONSPIRA.813e3519.woff"
+  }
+]);
